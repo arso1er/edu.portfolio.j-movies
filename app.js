6123ff11908710fa4df516caa12f36fd09ec85c9
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2022-04-04 17:10:19 +0100

    Replace myslq with mysql2 to get rid of authentication protocol error.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "jeff",
